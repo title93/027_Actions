c47b07e960e37310066fef35db51439260e3f300
title93 <titletukta2546@gmail.com>
2026-02-03 11:57:15 +0700

    Add unit tests for Calculator methods

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -1,13 +1,20 @@
 <?php
-namespace App;
-class Calculator
+use PHPUnit\Framework\TestCase;
+use App\Calculator;
+
+class CalculatorTest extends TestCase
 {
-  public function add(int $a, int $b): int
+  public function testAdd()
   {
-    return $a + $b;
+  $calc = new Calculator();
+  $this->assertEquals(5, $calc->add(2, 3));
+  $this->assertEquals(0, $calc->add(-1, 1));
   }
-  public function subtract(int $a, int $b): int
+  
+  public function testSubtract()
   {
-    return $a - $b;
+  $calc = new Calculator();
+  $this->assertEquals(1, $calc->subtract(3, 2));
+  $this->assertEquals(-2, $calc->subtract(-1, 1));
   }
 }
